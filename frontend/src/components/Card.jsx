81cc286844a8cf819171f0f84c54cdfc78788f62
koushik <koushikadepu1122@gmail.com>
2025-10-19 03:18:03 +0530

    new commit

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { userDataContext } from "../context/userContext";
+
+function Card({image}){
+
+     const {serverUrl, userData, setUserData, backendImage, setBackendImage, frontendImage, setFrontendImage,
+     selectedImage, setSelectedImage} = useContext(userDataContext)
+
+    return(
+        <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#030338] border-2 
+        border-[#03035ac8] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-cyan-600 
+        cursor-pointer hover:border-3 hover:border-white ${selectedImage ==image ? 
+        "border-3 border-white shadow-2xl shadow-cyan-600" : null}`} 
+        onClick={() => 
+            {
+                setSelectedImage(image)
+                setBackendImage(null)
+                setFrontendImage(null)
+                
+        }}>
+
+            <img src={image} className='h-full object-cover transition-transform duration-500
+        hover:scale-110 ' />
+
+        </div>
+    )
+}
+
+export default Card
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import bg from "../assets/bg.jpg"
+import { IoEye } from "react-icons/io5";
+import { useState } from "react";
+import { IoEyeOff } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { userDataContext } from "../context/userContext";
+import axios from "axios"
+
+
+function SignIn(){
+
+    const [showPassword, setShowPassword] = useState(false)
+
+    const {serverUrl, userData, setUserData} = useContext(userDataContext)
+    const navigate = useNavigate()
+
+     const [email, setEmail] = useState("")
+     const [loading, setLoading] = useState(false)
+     const [password, setPassword] = useState("")
+
+      const [err, setErr] = useState("")
+
+     const handleSignIn = async (e) =>{
+        e.preventDefault()
+        setErr("")
+        setLoading(true)
+        try {
+            let result = await axios.post(`${serverUrl}/api/auth/signin`, {
+            email, password
+      }, {withCredentials:true})
+
+      setUserData(result.data)
+      setLoading(false)
+      navigate("/")
+      
+
+        } catch (error) {
+            console.log(error)
+            setUserData(null)
+            setLoading(false)
+            setErr(error.response.data.message)
+        }
+
+     }
+
+    return (
+        <div className='w-full h-[100vh] bg-cover flex justify-center items-center '
+        style={{backgroundImage:`url(${bg})`}}>
+
+            <form className='w-[90%] h-[600px] max-w-[500px] bg-[#00000066] backdrop-blur 
+            shadow-[0_-10px_30px_0,0_10px_30px_0] shadow-cyan-400/40 flex flex-col justify-center 
+            items-center gap-[20px] px-[20px] ' onSubmit={handleSignIn}>
+
+                <h1 className='text-white text-[30px] font-semibold mb-[30px]'>
+                Sign In to<span className='text-blue-400 '> Virtual Assistant</span></h1>
+
+
+                <input type="email" placeholder='Email' className='w-full h-[60px] outline-none
+                border-2 border-white bg-transparent text-white placeholder-gray-300 px-[20px] py-[10px] 
+                text-[18px] rounded-full' required onChange={(e) => setEmail(e.target.value) } value={email}/>
+
+                <div className='w-full h-[60px] border-2 border-white bg-transparent text-white rounded-full
+                text-[18px] relative'>
+
+                    <input type={showPassword ? "text" : "password"} placeholder='Password' className='w-full h-full rounded-full outline-none
+                    bg-transparent placeholder-gray-300 px-[20px] py-[10px] ' required onChange={(e) => 
+                    setPassword(e.target.value) } value={password} />
+                    
+                    {!showPassword && <IoEye className='absolute top-[18px] right-[20px] w-[22px] h-[22px] 
+                    text-white cursor-pointer' onClick={() => setShowPassword(true)} />}
+
+                    {showPassword && <IoEyeOff className='absolute top-[18px] right-[20px] w-[22px] h-[22px] 
+                    text-white cursor-pointer' onClick={() => setShowPassword(false)} />}
+
+                </div>
+
+                {err.length>0 && <p className= 'text-red-500 text-[17px]'>
+                *{err}</p>}
+
+                <button className='min-w-[150px] h-[60px] mt-[30px] bg-white rounded-full text-black 
+                font-bold text-[19px] cursor-pointer' disabled={loading} >{loading ? "Loading..." : "Sign In"}</button>
+
+                <p className='text-white text-[16px] cursor-pointer' onClick={() => navigate("/signup")}>
+                Create a new account? <span className= 'text-cyan-300 text-[18px] font-semibold'> 
+                Sign Up</span></p>
+
+            </form>
+            
+        </div>
+    )
+}
+
+export default SignIn
