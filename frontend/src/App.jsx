6c5a2907e3afd879f35ac05a996c0b1b1907db90
Koushik adepu <92180851+Koushikadepu1122@users.noreply.github.com>
2025-10-19 04:04:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/signUp";
 import SignIn from "./pages/signIn";
 import Customize from "./pages/Customize";
-import { userDataContext } from "./context/userContext";
+import { userDataContext } from "./context/UserContext";
 import Home from "./pages/Home";
 import Customize2 from "./pages/Customize2";
 
